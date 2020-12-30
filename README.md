5f17105a22037939586e7952bffa3c067c6fee29
=GRUPOLTSYSTEM <Nelsonarrais9@gmail.com>
2020-12-30 17:29:10 -0200

    Impletmetação do BodyParser

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Express
 Nodemon
 Sequelize
 Mysql2
+Handlebars
--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 //Express
 const express = require ('express');
 const app = express();
-
-
-
-
-
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/html/index.html")
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('gdc', 'root', '123456789', {
+    host: "localhost",
+    dialect: 'mysql'
 });
 
 
+//config
+    // Template Engine
+ 
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    //body-parser
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
 
+//routes
 
+app.get('/cad', function(req, res){
+    res.render('formulario')
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.post('/add', function(req, res){
+  res.send("Texto: "+req.body.nome+" Conteudo:"+req.body.conteudo)
+});
 
 
 
